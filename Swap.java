6e2a39206ef7bd4fdd336bdf37acaa5347700cce
AmanYadav2369 <130037525+AmanYadav2369@users.noreply.github.com>
2023-04-20 12:04:06 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,17 @@
+/* wajp to swap two numbers using third variable. */
+
+class Swap
+{
+public static void main(String args[])
+{
+int a=20, b=30;
+System.out.println("a ="+a+"  "+"b ="+b);
+int c;
+
+c=a;
+a=b;
+b=c;
+System.out.println("a ="+a+"  "+"b ="+b);
+
+}
+}
